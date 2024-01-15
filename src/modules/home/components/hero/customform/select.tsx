3b7b0e70c62777b652a53d789474df22cf57fda7
gadsdencode <jordan.martens@osscontact.com>
2024-01-15 13:09:10 -0500

    more typescript fixes

--- src/modules/home/components/hero/customform/select.tsx
+++ src/modules/home/components/hero/customform/select.tsx
@@ -20,6 +20,7 @@ const Select: React.FC<SelectProps> = ({ children, ...props }) => (
 interface SelectTriggerProps extends React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger> {
   children?: React.ReactNode;  // Include children in the props type
   className?: string;
+  id?: string;
 }
 
 const SelectGroup = SelectPrimitive.Group
@@ -27,8 +28,8 @@ const SelectGroup = SelectPrimitive.Group
 const SelectValue = SelectPrimitive.Value
 
 const SelectTrigger = React.forwardRef<React.ElementRef<typeof SelectPrimitive.Trigger>, SelectTriggerProps>(
-  ({ className, children, ...props }, ref) => (
-    <SelectPrimitive.Trigger ref={ref} className={cn("flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1", className)} {...props}>
+  ({ className, id, children, ...props }, ref) => (
+    <SelectPrimitive.Trigger ref={ref} id={id} className={cn("flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1", className)} {...props}>
       {children}
       <SelectPrimitive.Icon asChild>
         <CaretSortIcon className="h-4 w-4 opacity-50" />
