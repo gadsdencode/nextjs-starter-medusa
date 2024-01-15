2a9497b6b738244256d21f26b0864ada732c232c
gadsdencode <jordan.martens@osscontact.com>
2024-01-15 14:43:02 -0500

    select component type fixes

--- src/modules/home/components/hero/customform/select.tsx
+++ src/modules/home/components/hero/customform/select.tsx
@@ -23,7 +23,17 @@ interface SelectTriggerProps extends React.ComponentPropsWithoutRef<typeof Selec
   id?: string;
 }
 
-interface SelectValueProps extends SelectPrimitive.SelectValueProps, React.RefAttributes<HTMLSpanElement> {}
+interface SelectValueProps extends SelectPrimitive.SelectValueProps, React.RefAttributes<HTMLSpanElement> {
+  children?: React.ReactNode;  // Include children in the props type
+  className?: string;
+  id?: string;
+}
+
+interface SelectContentProps extends React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content> {
+  children: React.ReactNode;
+  className?: string;
+  id?: string;
+}
 
 const SelectGroup = SelectPrimitive.Group
 
@@ -79,37 +89,25 @@ const SelectScrollDownButton = React.forwardRef<
 SelectScrollDownButton.displayName =
   SelectPrimitive.ScrollDownButton.displayName
 
-const SelectContent = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
-  <SelectPrimitive.Portal>
-    <SelectPrimitive.Content
-      ref={ref}
-      className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-        position === "popper" &&
-          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-        className
-      )}
-      position={position}
-      {...props}
-    >
-      <SelectScrollUpButton />
-      <SelectPrimitive.Viewport
-        className={cn(
-          "p-1",
-          position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
-        )}
-      >
-        {children}
-      </SelectPrimitive.Viewport>
-      <SelectScrollDownButton />
-    </SelectPrimitive.Content>
-  </SelectPrimitive.Portal>
-))
-SelectContent.displayName = SelectPrimitive.Content.displayName
+  const SelectContent = React.forwardRef<HTMLDivElement, SelectContentProps>(
+    ({ className, children, ...props }, ref) => (
+      <SelectPrimitive.Portal>
+        <SelectPrimitive.Content
+          ref={ref}
+          className={cn(
+            "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+            className // Updated to use cn function for merging class names
+          )}
+          {...props}
+        >
+          <SelectScrollUpButton />
+          <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+          <SelectScrollDownButton />
+        </SelectPrimitive.Content>
+      </SelectPrimitive.Portal>
+    )
+  );
+  SelectContent.displayName = 'SelectContent';
 
 const SelectLabel = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Label>,
