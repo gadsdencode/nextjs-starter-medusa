3dd9028554289843cdf66d1ef4edb6bb45ca2d7c
gadsdencode <jordan.martens@osscontact.com>
2024-01-16 05:19:33 -0500

    update popover

--- src/modules/home/components/hero/customform/popover.tsx
+++ src/modules/home/components/hero/customform/popover.tsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
-import * as PopoverPrimitive from '@radix-ui/react-popover';
+'use client'
+import * as React from "react"
+import * as PopoverPrimitive from "@radix-ui/react-popover"
 
-import { cn } from './utils';
+import { cn } from "./utils"
 
-const Popover = PopoverPrimitive.Root;
-const PopoverTrigger = PopoverPrimitive.Trigger;
-const PopoverAnchor = PopoverPrimitive.Anchor;
+const Popover = PopoverPrimitive.Root
+const PopoverTrigger = PopoverPrimitive.Trigger
+const PopoverAnchor = PopoverPrimitive.Anchor
 
-// Use React.ComponentProps instead of React.ComponentPropsWithoutRef
-type PopoverContentProps = React.ComponentProps<typeof PopoverPrimitive.Content>;
+// Adjusting the PopoverContentProps to align with the expected structure for React.forwardRef
+type PopoverContentProps = Omit<React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>, 'ref'>;
 
 const PopoverContent = React.forwardRef<HTMLDivElement, PopoverContentProps>(
   ({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
@@ -18,7 +19,7 @@ const PopoverContent = React.forwardRef<HTMLDivElement, PopoverContentProps>(
         align={align}
         sideOffset={sideOffset}
         className={cn(
-          "z-50 w-full rounded-lg bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+          'z-50 w-full rounded-lg bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
           className
         )}
         {...props}
@@ -28,4 +29,4 @@ const PopoverContent = React.forwardRef<HTMLDivElement, PopoverContentProps>(
 
 PopoverContent.displayName = 'PopoverContent';
 
-export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor };
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
