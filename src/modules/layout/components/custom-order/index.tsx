bff627e19929ac54ec44d1f8a18d4514093e7224
gadsdencode <jordan.martens@osscontact.com>
2024-01-16 11:20:02 -0500

    added placeholder prop

--- src/modules/layout/components/custom-order/index.tsx
+++ src/modules/layout/components/custom-order/index.tsx
@@ -6,7 +6,7 @@ import { Select, Option } from "@material-tailwind/react";
 export function SelectDefault() {
 return (
     <div className="w-72 m-5">
-    <Select className="rounded-lg text-white" id="heater-type" label="Select Version" nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
+    <Select className="rounded-lg text-white" id="heater-type" label="Select Version" placeholder={undefined} nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
         <Option className="text-black p-1" value="electric">Electric</Option>
         <Option className="text-black p-1" value="gas">Gas</Option>
         <Option className="text-black p-1" value="oil">Oil</Option>
