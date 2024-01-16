6174f709f49ee11de42f0f362a980990186f949e
gadsdencode <jordan.martens@osscontact.com>
2024-01-16 04:54:12 -0500

    updated label

--- src/modules/home/components/hero/customform/label.tsx
+++ src/modules/home/components/hero/customform/label.tsx
@@ -1,33 +1,28 @@
-"use client"
-
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "./utils"
+import * as React from 'react';
+import * as LabelPrimitive from '@radix-ui/react-label';
+import { cva } from 'class-variance-authority';
 
 const labelVariants = cva(
 "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+);
 
 type LabelProps = {
     children?: React.ReactNode;
     className?: string;
     htmlFor?: string;
-  } & Omit<React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>, 'ref'>;
+} & React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>;
 
-const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
-    ({ children, className, htmlFor, ...props }, ref) => (
+const Label: React.FC<LabelProps> = ({ children, className, htmlFor, ...props }) => {
+    return (
         <LabelPrimitive.Root
-            ref={ref}
             className={`${labelVariants} ${className || ''}`}
             htmlFor={htmlFor}
             {...props}
         >
             {children}
         </LabelPrimitive.Root>
-    )
-);
+    );
+};
 
 Label.displayName = 'Label';
 
